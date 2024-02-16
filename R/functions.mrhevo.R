d787ad80af9e6b4477ed14c82e357b82219dfd77
aiakvlie <andrii.iakovliev@ed.ac.uk>
2024-02-16 14:50:26 +0000

    Initial package setup.

--- R/functions.mrhevo.R
+++ R/functions.mrhevo.R
@@ -22,14 +22,14 @@ pnorm.extreme <- function(z, upper=TRUE) {
 #' reformat for LaTeX p-values that are in scientific notation
 #'
 #' @param x Character vector of numbers in scientific notation.
-#' @returns A character vector of LaTeX math expressions.  
+#' @returns A character vector of LaTeX math expressions.
 format.scinot.pvalue <- function(x) {
   x <- toupper(x)
   x.split <- as.numeric(unlist(strsplit(as.character(x), "E"))) # split x.split at E
   x.split <- signif(as.numeric(x.split, 1)) # 1 significant figure
   x.split <- t(matrix(x.split, nrow=2))
   ## handle cases where mantissa is rounded up to 10
-  roundedto10 <- x.split[, 1]==10  
+  roundedto10 <- x.split[, 1]==10
   x.split[, 1][roundedto10] <- 1
   x.split[, 2][roundedto10] <- x.split[, 2][roundedto10] - 1
   p.latex <- sprintf("\\ensuremath{%.*f \\times 10^{%0*d}}", 0, x.split[, 1], 0, x.split[, 2])
@@ -39,10 +39,10 @@ format.scinot.pvalue <- function(x) {
 #' generate formatted p-values from z values
 #'
 #' @param z Standard normal deviate.
-#' @param sigfig Number of significant figures to show in p-values. 
+#' @param sigfig Number of significant figures to show in p-values.
 #' @param neglogp.threshold.scinot Minus log10(pvalue) threshold for scientific notation.
 #' @param neglogp.threshold Minus log10(pvalue) cutoff for thresholding p-values.
-#' @param returns Vector of formatted p-values. 
+#' @param returns Vector of formatted p-values.
 format.z.aspvalue <- function(z, sigfig=1, neglogp.threshold.scinot=3, neglogp.threshold=NULL) {
 
   p <- signif(2 * pnorm(-abs(z)), sigfig)
@@ -99,7 +99,7 @@ get_summarystatsforMR <- function(Y, Z, X_u) {
     coeffs <- foreach(i = 1:ncol(Z),
                       .combine=function(...) rbind(..., fill = TRUE),
                       .multicombine = TRUE) %dopar% {
-                          scoreid <- colnames(Z)[i]   
+                          scoreid <- colnames(Z)[i]
                           formula.string <- paste0("y ~ ",
                                                    paste(colnames(X_u), collapse=" + "),
                                                    " + ", scoreid)
@@ -113,6 +113,11 @@ get_summarystatsforMR <- function(Y, Z, X_u) {
     return(coeffs)
 }
 
+#' calculate ratios of coefficients
+#' @param coeffs.dt
+#' @returns Input data.table plus a column containing ratios of coefficients
+#'               for each genetic instrument.
+#' @export
 get_coeffratios <- function(coeffs.dt, use.delta=FALSE) {
     if(use.delta) {
         ## second-order Taylor expansions (delta method)
@@ -123,7 +128,7 @@ get_coeffratios <- function(coeffs.dt, use.delta=FALSE) {
         coeffs.dt[, theta_IV := gamma_hat / alpha_hat]  # ratio estimates
         coeffs.dt[, se.theta_IV := se.gamma_hat / alpha_hat]
     }
-    coeffs.dt[, size.theta_IV := 0.3 * sum(se.theta_IV) / se.theta_IV] 
+    coeffs.dt[, size.theta_IV := 0.3 * sum(se.theta_IV) / se.theta_IV]
     coeffs.dt[, inv.var := se.theta_IV^-2]
     return(coeffs.dt)
 }
@@ -131,12 +136,12 @@ get_coeffratios <- function(coeffs.dt, use.delta=FALSE) {
 #' calculate conventional MR estimators from summary stats
 #'
 #' @param coeffs.dt Data.table with columns for coefficient and SE of regression of exposure on instrument (alpha_hat, se.alpha_hat), regression of outcome on instrument (gamma_hat, se.gamma_hat) and coefficient ratios (theta_IV, se.theta_IV).
-#' @returns Table of estimates for weighted mean, weighted median, and penalized weighted median of instrumental variable estimates. 
+#' @returns Table of estimates for weighted mean, weighted median, and penalized weighted median of instrumental variable estimates.
 get_estimatorsMR <- function(coeffs.dt) {
     ## IVW estimator
-    theta_IVW <- coeffs.dt[, sum(theta_IV * inv.var) / sum(inv.var)] 
+    theta_IVW <- coeffs.dt[, sum(theta_IV * inv.var) / sum(inv.var)]
     se.theta_IVW  <- coeffs.dt[, sqrt(1 / sum(inv.var))]
-    
+
     ## weighted median estimator
     thetaWM <- coeffs.dt[, matrixStats::weightedMedian(x=theta_IV, w=inv.var)]
     se.thetaWM <- coeffs.dt[, weighted.median.boot(alpha_hat, gamma_hat, se.alpha_hat, se.gamma_hat, inv.var)]
@@ -147,14 +152,14 @@ get_estimatorsMR <- function(coeffs.dt) {
     coeffs.dt[, penalty := pchisq(inv.var * (theta_IV - theta_IVW)^2, df=1, lower.tail=FALSE)]
     ## Bowden et al use 20 instead of 20 * .N (equivalent to ignoring penalty where pchisq > .05)
     coeffs.dt[, penalized.weights := inv.var * pmin(1, penalty * 20 *.N)]  # penalized weights
-    thetaPWM <- coeffs.dt[, matrixStats::weightedMedian(x=theta_IV, w=penalized.weights)] 
+    thetaPWM <- coeffs.dt[, matrixStats::weightedMedian(x=theta_IV, w=penalized.weights)]
     se.thetaPWM <- coeffs.dt[, weighted.median.boot(alpha_hat, gamma_hat,
                                                  se.alpha_hat, se.gamma_hat,
                                                  penalized.weights)]
-    
+
     estimators.dt <- data.table(Estimator=c("Weighted mean", "Weighted median",
                                             "Penalized weighted median"),
-                                Estimate=c(theta_IVW, thetaWM, thetaPWM), 
+                                Estimate=c(theta_IVW, thetaWM, thetaPWM),
                                 SE=c(se.theta_IVW, se.thetaWM, se.thetaPWM))
     estimators.dt[, z := Estimate / SE]
     estimators.dt[, pvalue := 2 * pnorm(-abs(z))]
@@ -166,11 +171,11 @@ get_estimatorsMR <- function(coeffs.dt) {
 #' @param x Numeric vector of samples from the posterior distribution of the parameter.
 #' @param prior Numeric vector of values of the prior density for each element of x.
 #' @param return.asplot Logical value determines whether the function returns maximum likelihood estimate or a plot of the posterior density and log-likelihood.
-#' @returns If return.asplot is FALSE, data.table with one row containing maximum likelihood estimate, standard error, test statistic, p-value, formatted p-value. 
+#' @returns If return.asplot is FALSE, data.table with one row containing maximum likelihood estimate, standard error, test statistic, p-value, formatted p-value.
 mle.se.pval <- function(x, prior, return.asplot=FALSE) {
   require(data.table)
   require(ggplot2)
-  
+
   invprior <- 1 / prior
   invprior <- invprior / sum(invprior)
 
@@ -204,7 +209,7 @@ mle.se.pval <- function(x, prior, return.asplot=FALSE) {
                                        "'logl.fit'='Smoothed curve'; 'logl.quad'='Fitted quadratic'", as.factor=TRUE)]
     p.loglik <- ggplot(loglik.long, aes(x=x, y=loglik, color=curve)) +
       geom_line() +
-      xlab("Parameter value") + 
+      xlab("Parameter value") +
       ylab("Log-likelihood") +
       theme(legend.position=c(0.5, 0.5)) +
       theme(legend.title=element_blank())
@@ -213,10 +218,10 @@ mle.se.pval <- function(x, prior, return.asplot=FALSE) {
     fitted.dt <- data.table(x=fitted$x, posterior=fitted$y)
     p.density <- ggplot(fitted.dt, aes(x=x, y=posterior)) +
       geom_line() +
-      xlab("Parameter value") + 
-      ylab("Smoothed posterior density") + 
+      xlab("Parameter value") +
+      ylab("Smoothed posterior density") +
       scale_x_continuous(limits=xlimits)
-    
+
     p.bayesloglik <- cowplot::plot_grid(p.density, p.loglik, nrow=2)
     return(p.bayesloglik)
   } else {
@@ -229,14 +234,18 @@ mle.se.pval <- function(x, prior, return.asplot=FALSE) {
 #' @param sampling If TRUE, uses sampling rather than variational approximation.
 #' @param logistic If TRUE, uses logistic rather than linear regression.
 #' @param Z Data.table of genetic instruments..
-#' @param Y Vector of outcomes.  
+#' @param Y Vector of outcomes.
 #' @param sigma_y Standard deviation of outcome variable, used only if logistic=FALSE.
 #' @param X_u Data.table of unpenalized covariates.
-#' @param alpha_hat Vector of estimated coefficients for effect of instruments on exposure.  
+#' @param alpha_hat Vector of estimated coefficients for effect of instruments on exposure.
 #' @param se.alpha_hat Vector of standard errors for coefficients alpha_hat.
-#' @param fraction_pleio Prior guess at fraction of instruments that have pleiotropic effects: values between 0.05 and 0.95 are allowed. 
+#' @param fraction_pleio Prior guess at fraction of instruments that have pleiotropic effects: values between 0.05 and 0.95 are allowed.
 #' @param priorsd_theta Standard deviation of prior on theta.
-#' @returns An object of class stanfit. 
+#' @returns An object of class stanfit.
+#'
+#' @example man/examples/runmrhevo.R
+#' @export
+#' @import data.table rstan ggplot2
 run_mrhevo <- function(use.sampling=TRUE, logistic=TRUE, Z, Y, sigma_y=1, X_u, alpha_hat, se.alpha_hat, fraction_pleio=NULL, slab_scale=0.25, priorsd_theta=1, vb.algo="meanfield") {
   require(rstan)
 
@@ -257,23 +266,23 @@ run_mrhevo <- function(use.sampling=TRUE, logistic=TRUE, Z, Y, sigma_y=1, X_u, a
   J <- ncol(Z)
   X_u <- scale(X_u, center=TRUE, scale=TRUE)
   Z <- scale(Z, center=TRUE, scale=FALSE)
-  
+
   ## priors
   scale_intercept_y <- 10  # weak prior on Y intercept
   scale_beta_u <- 1 # prior sd of coeffs for unpenalized covariates X_u
   ## prior scale of c is slab_scale
   slab_df <- 2    # 1 for half-Cauchy, large value specifies a gaussian prior on slab component
-  nu_global <- 1  # 1 for half-Cauchy prior on global scale param: specifying a larger value will limit the narrowness of the spike component 
+  nu_global <- 1  # 1 for half-Cauchy prior on global scale param: specifying a larger value will limit the narrowness of the spike component
   nu_local <- 1    # 1 for half-Cauchy, horseshoe+ or horseshoe if c is large
-  
+
   if(is.null(fraction_pleio)) {
     fraction_pleio <- 0.5 # prior guess of number of instruments that are pleiotropic
-  } 
+  }
   r_pleio <- fraction_pleio * J
-  
+
   ## Piironen and Vehtari recommend that the prior on tau should be chosen to have most of the prior mass
   ## near (r_pleio / (N * (J - r_pleio)) * sqrt(pseudovariance) / sqrt(N),
-  ## where r_pleio is a prior guess for the number of nonzero coefficients 
+  ## where r_pleio is a prior guess for the number of nonzero coefficients
 
   ## prior median of a half-t distribution with nu_global df
   priormedian <- qt(p=0.75, df=nu_global, lower.tail=TRUE) # 0.82 with nu_global=1)
@@ -286,11 +295,11 @@ run_mrhevo <- function(use.sampling=TRUE, logistic=TRUE, Z, Y, sigma_y=1, X_u, a
   }
   ## choose prior on tau so that most of the prior mass is near tau_0
   ## choose scale_global so that the prior median of the half-t distribution equates to tau_0
-  scale_global <- tau_0 / priormedian 
+  scale_global <- tau_0 / priormedian
 
   ## sample the posterior
   data.stan <- list(logistic=as.integer(logistic),
-                    Z=as.matrix(Z), Y=Y, X_u=as.matrix(X_u),   
+                    Z=as.matrix(Z), Y=Y, X_u=as.matrix(X_u),
                     N=N, J=J, U=ncol(X_u),
                     alpha_hat=alpha_hat,
                     sd_alpha_hat=se.alpha_hat,
@@ -311,7 +320,7 @@ run_mrhevo <- function(use.sampling=TRUE, logistic=TRUE, Z, Y, sigma_y=1, X_u, a
                               chains=4,
                               refresh=200,
                               control=list(adapt_delta=0.95),
-                              verbose=FALSE) 
+                              verbose=FALSE)
   } else {
     fit.mc <- rstan::vb(object=mr.stanmodel,
                         data=data.stan,
@@ -320,41 +329,50 @@ run_mrhevo <- function(use.sampling=TRUE, logistic=TRUE, Z, Y, sigma_y=1, X_u, a
                         iter=20000,
                         adapt_engaged=TRUE,
                         tol_rel_obj=0.01)
-  }    
+  }
   return(fit.mc)
 }
 
-#' run Stan model for Mendelian randomization with regularized horsehoe prior on pleiotropic effects, using summary statistics only. 
-#' @param alpha_hat Vector of estimated coefficients for effect of instruments on exposure.  
+#' run Stan model for Mendelian randomization with regularized horsehoe prior on pleiotropic effects, using summary statistics only.
+#' @param alpha_hat Vector of estimated coefficients for effect of instruments on exposure.
 #' @param se.alpha_hat Vector of standard errors for coefficients alpha_hat.
-#' @param gamma_hat Vector of estimated coefficients for effect of instruments on outcome.  
+#' @param gamma_hat Vector of estimated coefficients for effect of instruments on outcome.
 #' @param se.gamma_hat Vector of standard errors for coefficients gamma_hat.
-#' @param fraction_pleio Prior guess at fraction of instruments that have pleiotropic effects: values between 0.05 and 0.95 are allowed. 
+#' @param fraction_pleio Prior guess at fraction of instruments that have pleiotropic effects: values between 0.05 and 0.95 are allowed.
 #' @param slab_scale scale param of prior on direct effects.
 #' @param priorsd_theta Standard deviation of prior on theta.
+<<<<<<< ours:functions.mrhevo.R
 #' @returns An object of class stanfit. 
 run_mrhevo.sstats <- function(alpha_hat, se.alpha_hat, gamma_hat, se.gamma_hat, fraction_pleio=0.5, slab_scale=0.2, slab_df=2, priorsd_theta=1) {
+||||||| ancestor
+#' @returns An object of class stanfit. 
+run_mrhevo.sstats <- function(alpha_hat, se.alpha_hat, gamma_hat, se.gamma_hat, fraction_pleio=NULL, slab_scale=0.2, slab_df=2, priorsd_theta=1) {
+=======
+#' @returns An object of class stanfit.
+#' @export
+run_mrhevo.sstats <- function(alpha_hat, se.alpha_hat, gamma_hat, se.gamma_hat, fraction_pleio=NULL, slab_scale=0.2, slab_df=2, priorsd_theta=1) {
+>>>>>>> theirs:R/functions.mrhevo.R
   require(rstan)
   options(mc.cores = parallel::detectCores())
   rstan_options(auto_write = TRUE)
-  
+
   cat("compiling stan model ... ")
   mr.sstats.stanmodel <- stan_model(file="../mrhevo/MRHevo_summarystats.stan",
                                     model_name="MRHevo.summarystats", verbose=FALSE)
   cat("done\n")
-  
+
   ## check arguments for consistency
   stopifnot(length(alpha_hat)==length(gamma_hat))
   stopifnot(fraction_pleio >= 0.01 & fraction_pleio <= 0.99)
-  
+
   J <- length(alpha_hat)
-  
+
   var.theta_IV_delta <- (se.gamma_hat / alpha_hat)^2 +
     gamma_hat^2 * se.alpha_hat^2 / alpha_hat^4 # Taylor expansion
   info <- mean( 1 / var.theta_IV_delta) # mean Fisher info for IV estimate
 
   ## df of half-t priors
-  nu_global <- 1  # 1 for half-Cauchy prior on global scale param: specifying a larger value will limit the narrowness of the spike component 
+  nu_global <- 1  # 1 for half-Cauchy prior on global scale param: specifying a larger value will limit the narrowness of the spike component
   nu_local <- 1    # 1 for half-Cauchy, horseshoe+ or horseshoe if c is large
 
 
@@ -367,7 +385,7 @@ run_mrhevo.sstats <- function(alpha_hat, se.alpha_hat, gamma_hat, se.gamma_hat, 
   priormedian <- qt(p=0.75, df=nu_global, lower.tail=TRUE) # 0.82 with nu_global=1)
   scale_global <- tau0 / priormedian
 
-  data.stan <- list(J=length(alpha_hat), 
+  data.stan <- list(J=length(alpha_hat),
                     gamma_hat=gamma_hat,
                     sd_gamma_hat=se.gamma_hat,
                     alpha_hat=alpha_hat,
@@ -392,15 +410,16 @@ run_mrhevo.sstats <- function(alpha_hat, se.alpha_hat, gamma_hat, se.gamma_hat, 
   return(fit.mc)
 }
 
-#' run Stan model for Mendelian randomization with regularized horsehoe prior on pleiotropic effects, using summary statistics only. 
-#' @param alpha_hat Vector of estimated coefficients for effect of instruments on exposure.  
+#' run Stan model for Mendelian randomization with regularized horsehoe prior on pleiotropic effects, using summary statistics only.
+#' @param alpha_hat Vector of estimated coefficients for effect of instruments on exposure.
 #' @param se.alpha_hat Vector of standard errors for coefficients alpha_hat.
-#' @param gamma_hat Vector of estimated coefficients for effect of instruments on outcome.  
+#' @param gamma_hat Vector of estimated coefficients for effect of instruments on outcome.
 #' @param se.gamma_hat Vector of standard errors for coefficients gamma_hat.
-#' @param fraction_pleio Prior guess at fraction of instruments that have pleiotropic effects: values between 0.05 and 0.95 are allowed. 
+#' @param fraction_pleio Prior guess at fraction of instruments that have pleiotropic effects: values between 0.05 and 0.95 are allowed.
 #' @param slab_scale scale param of prior on direct effects.
 #' @param priorsd_theta Standard deviation of prior on theta.
-#' @returns An object of class stanfit. 
+#' @returns An object of class stanfit.
+#' @export
 run_mrhevo.fixedtau <- function(alpha_hat, se.alpha_hat, gamma_hat, se.gamma_hat, tau=1E-6, slab_scale=0.2, slab_df=2, priorsd_theta=1) {
   require(rstan)
   options(mc.cores = parallel::detectCores())
@@ -408,23 +427,23 @@ run_mrhevo.fixedtau <- function(alpha_hat, se.alpha_hat, gamma_hat, se.gamma_hat
 
   ## check arguments for consistency
   stopifnot(length(alpha_hat)==length(gamma_hat))
-  
+
   cat("compiling stan model ... ")
   mr.fixedtau.stanmodel <- stan_model(file="../mrhevo/MRHevo_fixedtau.stan",
                                     model_name="MRHevo.fixedtau", verbose=FALSE)
   cat("done\n")
-  
+
   J <- length(alpha_hat)
-  
+
   ## priors
   nu_local <- 1    # 1 for half-Cauchy, horseshoe+ or horseshoe if c is large
 
-  data.stan <- list(J=length(alpha_hat), 
+  data.stan <- list(J=length(alpha_hat),
                     gamma_hat=gamma_hat,
                     sd_gamma_hat=se.gamma_hat,
                     alpha_hat=alpha_hat,
                     sd_alpha_hat=se.alpha_hat,
-                    tau=tau, 
+                    tau=tau,
                     nu_local=nu_local,
                     priorsd_theta=priorsd_theta,
                     slab_scale=slab_scale,
@@ -444,21 +463,21 @@ run_mrhevo.fixedtau <- function(alpha_hat, se.alpha_hat, gamma_hat, se.gamma_hat
 }
 
 #' get value tau0 for global shrinkage parameter tau given expectation of fraction_pleio
-#' @param fraction_pleio Prior guess at fraction of effects that are nonzero.  
-#' @param nu_global Shape parameter of gamma prior.  
+#' @param fraction_pleio Prior guess at fraction of effects that are nonzero.
+#' @param nu_global Shape parameter of gamma prior.
 #' @param J number of variables. .
 #' @param pseudovariance Variance of outcome variable
-#' @returns Value of tau0. 
+#' @returns Value of tau0.
 set.tau0 <- function(fraction_pleio=NULL, nu_global=1, J, info) {
   if(is.null(fraction_pleio)) {
     fraction_pleio <- 0.5 # prior guess of number of instruments that are pleiotropic
   }
   r_pleio <- fraction_pleio * J
-  
+
   ## Piironen and Vehtari recommend that the prior on tau should be chosen to have most of the prior mass
   ## near (r_pleio / (N * (J - r_pleio)) * sqrt(pseudovariance) / sqrt(N),
-  ## where r_pleio is a prior guess for the number of nonzero coefficients 
-  
+  ## where r_pleio is a prior guess for the number of nonzero coefficients
+
   tau0 <- (r_pleio / (J - r_pleio)) / sqrt(info)
   return(tau0)
 }
--- man/examples/runmrhevo.R
+++ man/examples/runmrhevo.R
@@ -2,7 +2,7 @@
 ## this script runs a complete MRHevo analysis of the exemplar dataset coeffs.dt, with various tables and plots reporting the results
 
 ## the functions are in ./functions.mrhevo.R with documentation in correct format for R package
-## I suggest making a package so that running the Stan model creates a a class mrhevo.stanfit. 
+## I suggest making a package so that running the Stan model creates a a class mrhevo.stanfit.
 ## This should contain the stanfit object hevo.stanfit and the data.table object coeffs.dt
 
 ## all the chunks of code called to generate tables and plots should be methods for this object
@@ -13,16 +13,22 @@ library(rstan)
 library(ggrepel)
 library(ggplotify)
 
-source("../mrhevo/functions.mrhevo.R")
-
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
 
 ## exemplar dataset to be included in package
 ## dataset should have columns named qtlname, alpha_hat, se.alpha_hat, gamma_hat, se.gamma_hat
 ## other columns are optional
+<<<<<<< ours:runmrhevo.R
 coeffs.dt <- readRDS("./coeffs_ADIPOQ_UKBB.RDS")
 #coeffs.dt <- readRDS("../mrhevo/coeffs_CLC_asthma_UKBB.RDS")
+||||||| ancestor
+coeffs.dt <- readRDS("./coeffs_ADIPOQ_UKBB.RDS")
+coeffs.dt <- readRDS("../mrhevo/coeffs_CLC_asthma_UKBB.RDS")
+=======
+# coeffs.dt <- readRDS("./coeffs_ADIPOQ_UKBB.RDS")
+# coeffs.dt <- readRDS("../mrhevo/coeffs_CLC_asthma_UKBB.RDS")
+>>>>>>> theirs:man/examples/runmrhevo.R
 #coeffs.dt <- coeffs.dt[minpvalue < 1E-8]
 ## calculate coefficient ratios for each instrument
 coeffs.dt <- get_coeffratios(coeffs.dt, use.delta=TRUE)
@@ -44,7 +50,7 @@ hevo.stanfit <-
                       gamma_hat=coeffs.dt$gamma_hat,
                       se.gamma_hat=coeffs.dt$se.gamma_hat,
                       slab_scale=slab_scale,
-                      slab_df=slab_df, 
+                      slab_df=slab_df,
                       priorsd_theta=1)
 options(warn=2)
 
@@ -79,8 +85,8 @@ options(warn=2)
 pars.forsummary <- c("theta", "log_c", "log_tau", "f", "lp__", "beta", "kappa")
 
 fit.coeffs <- summary(hevo.stanfit,
-                      pars=pars.forsummary, 
-                      probs=c(0.1, 0.9))$summary 
+                      pars=pars.forsummary,
+                      probs=c(0.1, 0.9))$summary
 fit.coeffs <- as.data.table(round(fit.coeffs, 3), keep.rownames="variable")
 fit.coeffs <- fit.coeffs[, .(variable, mean, `10%`, `90%`, n_eff, Rhat)]
 fit.coeffs[, n_eff := round(n_eff, 2)]
@@ -88,7 +94,7 @@ fit.coeffs[, n_eff := round(n_eff, 2)]
 qtlnames <- coeffs.dt$qtlname
 fit.coeffs[grep("beta\\[", variable), qtlname := ..qtlnames]
 fit.coeffs[grep("kappa\\[", variable), qtlname := ..qtlnames]
-fit.coeffs[qtlname=="", qtlname := variable]  
+fit.coeffs[qtlname=="", qtlname := variable]
 fit.coeffs
 
 ## plot shrinkage coefficients
@@ -99,7 +105,7 @@ p.shrinkage
 
 p.beta <- ggplot(data=fit.coeffs[grep("beta", variable)],
                       aes(y=qtlname, x=mean, xmin=`10%`, xmax=`90%`)) +
-    geom_pointrange() + 
+    geom_pointrange() +
     geom_vline(xintercept=0, linetype="dotted")
 p.beta
 
@@ -110,18 +116,18 @@ estimators <- rbind(estimators, mle.theta, fill=TRUE)
 
 ## plot coefficients and show estimators as slopes of lines through origin
 p.coeffs <- ggplot(coeffs.dt,
-                       aes(x=alpha_hat, y=gamma_hat)) + 
+                       aes(x=alpha_hat, y=gamma_hat)) +
     geom_point(aes(size=size.theta_IV), alpha=0.8) +
-    scale_size(guide="none") + 
-    ggrepel::geom_text_repel(aes(label=qtlname), force=5, size=2.5, fontface="italic", color="blue") + 
-    scale_x_continuous(limits = c(0, NA), expand = expansion(mult = c(0, 0.1))) + 
+    scale_size(guide="none") +
+    ggrepel::geom_text_repel(aes(label=qtlname), force=5, size=2.5, fontface="italic", color="blue") +
+    scale_x_continuous(limits = c(0, NA), expand = expansion(mult = c(0, 0.1))) +
     scale_y_continuous(limits = c(min(c(coeffs.dt$gamma_hat, 0)),
                                   max(c(coeffs.dt$gamma_hat, 0))),
-                       expand =  expansion(mult = c(0.1, 0.1))) + 
+                       expand =  expansion(mult = c(0.1, 0.1))) +
     geom_abline(data=estimators,
-                aes(slope=Estimate, intercept=rep(0, nrow(estimators)), 
+                aes(slope=Estimate, intercept=rep(0, nrow(estimators)),
                     linetype=Estimator)) +
-    labs(linetype="Estimate of causal effect as slope of line through origin") + 
+    labs(linetype="Estimate of causal effect as slope of line through origin") +
     theme(legend.position="top") +
     theme(legend.direction="vertical") +
     theme(legend.box="horizontal") +
